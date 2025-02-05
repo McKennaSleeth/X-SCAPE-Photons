191d05440bd1ffefa927740859b9e7efe0c47bd4
McKenna R Sleeth <131323470+McKennaSleeth@users.noreply.github.com>
2025-02-04 20:56:56 -0600

    Update pionpluscross_secv4.cpp

--- pionpluscross_secv4.cpp
+++ pionpluscross_secv4.cpp
@@ -64,7 +64,7 @@ int main() {
 
 
     std::string filename = "pion_pp_pTHat15_30_out_final_state_hadrons.dat";
-    const double pTlist[] = {5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0};
+    const double pTlist[] = {5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0};
         const int npT = sizeof(pTlist)/sizeof(double) - 1;
 
     double sigmaGen = 0.0;
